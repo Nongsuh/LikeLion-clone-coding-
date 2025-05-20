afea2eb664d3c0799cf55c13373d54eb04bbeb90
Nongsuh <20231350@sungshin.ac.kr>
2025-05-21 03:17:01 +0900

    Add files via upload

---
+++ Section04.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Section04 = () => {
+    return (
+        <div className='Section04_wrap'>
+        <h3><em>CORE-VALUES</em></h3>
+        <h3>GROUND-RULES</h3>
+        
+
+        <div>
+            <div>
+                <h3>Comitiment</h3>
+                <p>
+                    정직: Honesty <br/>
+                    존중: Respect <br/>
+                    신뢰: Trust
+                </p>
+            </div>
+            <div>
+                <h3>Challenge</h3>
+                <div className="orange">
+                    <p>
+                    개척: Pioneer <br/>
+                    집착: Obesession
+                    </p>
+                </div>
+                <p>
+                    탁월: Excellence
+                </p>
+            </div>
+            <div>
+                <h3>Companionship</h3>
+                <p>
+                    다양성: Diversity <br/>
+                    배려: Consideration <br/>
+                    소통: Communication
+                </p>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Section04
